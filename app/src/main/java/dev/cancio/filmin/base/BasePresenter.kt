db30bf28cae28647fbb910f4ef056f0404c22d20
cancio <filipe.cancio@outlook.com>
2021-08-31 11:48:30 -0300

    Fix lifecycle on Base Presenter

--- app/src/main/java/dev/cancio/filmin/base/BasePresenter.kt
+++ app/src/main/java/dev/cancio/filmin/base/BasePresenter.kt
@@ -1,9 +1,10 @@
 package dev.cancio.filmin.base
 
 import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.LifecycleObserver
 import androidx.lifecycle.OnLifecycleEvent
 
-abstract class BasePresenter<T: BaseView>(private val view: T){
+abstract class BasePresenter<T: BaseView>(private val view: T): LifecycleObserver{
 
     @OnLifecycleEvent(Lifecycle.Event.ON_CREATE)
     fun onStartView(){
