e12e358f68b28e60db17fbda2949ba88ad065038
cancio <filipe.cancio@outlook.com>
2021-09-09 16:22:28 -0300

    Remove @Named decorator #14

--- app/src/main/java/dev/cancio/filmin/di/MainModule.kt
+++ app/src/main/java/dev/cancio/filmin/di/MainModule.kt
@@ -4,13 +4,12 @@ import dagger.Module
 import dagger.Provides
 import dev.cancio.filmin.data.service.MovieService
 import retrofit2.Retrofit
-import javax.inject.Named
 
 @Module
 class MainModule {
 
     @Provides
-    fun provideMovie(@Named(RetrofitModule.RETROFIT) retrofit: Retrofit): MovieService {
+    fun provideMovie(retrofit: Retrofit): MovieService {
         return retrofit.create(MovieService::class.java)
     }
 }
--- app/src/main/java/dev/cancio/filmin/di/RetrofitModule.kt
+++ app/src/main/java/dev/cancio/filmin/di/RetrofitModule.kt
@@ -2,24 +2,16 @@ package dev.cancio.filmin.di
 
 import dagger.Module
 import dagger.Provides
-import dagger.Subcomponent
 import dev.cancio.filmin.BuildConfig
-import dev.cancio.filmin.ui.activity.HomeActivity
 import okhttp3.OkHttpClient
 import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
-import javax.inject.Named
 
 @Module
 class RetrofitModule {
 
-    companion object{
-        const val RETROFIT = "RETROFIT"
-    }
-
     @Provides
-    @Named(RETROFIT)
     fun provideRetrofit(): Retrofit {
 
         val client = OkHttpClient.Builder()
