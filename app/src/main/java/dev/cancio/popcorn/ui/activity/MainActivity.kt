dc05c08f03b01926994f0e60f5b1d17be4520b4a
cancio <filipe.cancio@outlook.com>
2021-09-14 17:28:36 -0300

    Criando rotas de navegação em fragments

--- app/src/main/java/dev/cancio/popcorn/ui/activity/MainActivity.kt
+++ app/src/main/java/dev/cancio/popcorn/ui/activity/MainActivity.kt
@@ -2,15 +2,19 @@ package dev.cancio.popcorn.ui.activity
 
 import android.content.Context
 import android.os.Bundle
+import android.view.MenuItem
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import com.google.android.material.bottomnavigation.BottomNavigationView
 import dev.cancio.popcorn.MyApplication
+import dev.cancio.popcorn.R
 import dev.cancio.popcorn.data.repository.MovieRepository
 import dev.cancio.popcorn.data.model.MoviePagination
 import dev.cancio.popcorn.databinding.ActivityMainBinding
 import dev.cancio.popcorn.ui.adapter.MovieItemAdapter
+import dev.cancio.popcorn.ui.fragment.HomeFragment
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
@@ -28,11 +32,14 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         val binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        startFragment()
+
+        val bottomNavigationView = binding.bottomNavigationView
         MyApplication().appComponent.inject(this)
-        getMoviesList(binding,this)
+        //getMoviesList(binding,this)
     }
 
-    private fun getMoviesList(binding: ActivityMainBinding,context:Context){
+    /*private fun getMoviesList(binding: ActivityMainBinding,context:Context){
         movieApi.getMoviesList().enqueue(object : Callback<MoviePagination>{
             override fun onResponse(
                 call: Call<MoviePagination>,
@@ -60,10 +67,18 @@ class MainActivity : AppCompatActivity() {
             }
 
         })
+    }*/
+
+    private fun startFragment() {
+        val homeFragment = HomeFragment()
+        supportFragmentManager.beginTransaction()
+            .add(R.id.nav_host_fragment, homeFragment)
+            .commit()
     }
 
     private fun onError() {
         Toast.makeText(this, "A conexão falhou", Toast.LENGTH_SHORT).show()
     }
+
 
 }
