abaa98668125be58f36b71440ec6b58b329800a6
cancio <filipe.cancio@outlook.com>
2021-09-15 07:16:19 -0300

    fix adjustments on bottom navigation #18

--- app/src/main/java/dev/cancio/popcorn/ui/activity/MainActivity.kt
+++ app/src/main/java/dev/cancio/popcorn/ui/activity/MainActivity.kt
@@ -5,8 +5,13 @@ import android.os.Bundle
 import android.view.MenuItem
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
+import androidx.navigation.NavController
+import androidx.navigation.Navigation
+import androidx.navigation.ui.setupWithNavController
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import com.google.android.material.bottomappbar.BottomAppBar
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import dev.cancio.popcorn.MyApplication
 import dev.cancio.popcorn.R
@@ -15,6 +20,8 @@ import dev.cancio.popcorn.data.model.MoviePagination
 import dev.cancio.popcorn.databinding.ActivityMainBinding
 import dev.cancio.popcorn.ui.adapter.MovieItemAdapter
 import dev.cancio.popcorn.ui.fragment.HomeFragment
+import dev.cancio.popcorn.ui.fragment.LikeFragment
+import dev.cancio.popcorn.ui.fragment.NoneFragment
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
@@ -27,16 +34,25 @@ class MainActivity : AppCompatActivity() {
 
     private lateinit var recyclerView: RecyclerView
     private lateinit var movieItemAdapter: MovieItemAdapter
+    lateinit var navController : NavController
+    lateinit var bottomNavigationView: BottomNavigationView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         val binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
-        startFragment()
+        //startHomeFragment()
+        setNavigationController()
 
-        val bottomNavigationView = binding.bottomNavigationView
         MyApplication().appComponent.inject(this)
         //getMoviesList(binding,this)
+    }
+
+    private fun setNavigationController(){
+        navController = Navigation.findNavController(this,R.id.nav_host_fragment)
+        bottomNavigationView = findViewById(R.id.bottom_nav_bar)
+        bottomNavigationView.setupWithNavController(navController)
+
     }
 
     /*private fun getMoviesList(binding: ActivityMainBinding,context:Context){
@@ -69,11 +85,18 @@ class MainActivity : AppCompatActivity() {
         })
     }*/
 
-    private fun startFragment() {
+    private fun startHomeFragment() {
         val homeFragment = HomeFragment()
         supportFragmentManager.beginTransaction()
             .add(R.id.nav_host_fragment, homeFragment)
             .commit()
+    }
+
+    private fun replaceFragment(fragmentId: Int, fragment: Fragment): Boolean {
+        supportFragmentManager.beginTransaction()
+            .replace(fragmentId, fragment)
+            .commit()
+        return true
     }
 
     private fun onError() {
